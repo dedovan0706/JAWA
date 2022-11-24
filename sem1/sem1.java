7c391bf76d5cff41db856cce8d593f335737b344
dedov <dedovan0706@gail.com>
2022-11-24 11:27:12 +0700

    Add task1

---
+++ sem1/sem1.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+public class sem1 {
+   public static void main(String[] args) {
+    //psvm или main - быстрый набор метода main
+        System.out.println("Hello world");
+        System.out.println(LocalDateTime.now());
+   } 
+}
--- sem1/task2.java
+++ sem1/task2.java
@@ -1,3 +1,9 @@
 public class task2 {
+    public static void main (String[] args) {
+        System.out.println("привет мир");
+       }
+    }
+    
+    
     
 }
