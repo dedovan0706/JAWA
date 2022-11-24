0ae81756ee1f9dd82a5779742c2b36bf764b9bad
dedov <dedovan0706@gail.com>
2022-11-24 14:11:10 +0700

    Add task2

--- java_project/sem1.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDateTime;
-public class sem1 {
-   public static void main(String[] args) {
-    //psvm или main - быстрый набор метода main
-        System.out.println("Hello world");
-        System.out.println(LocalDateTime.now());
-   } 
-}
--- sem1/task2.java
+++ sem1/task2.java
@@ -1,9 +1,27 @@
+/*Дан массив двоичных чисел, например [1,1,0,1,1,1], 
+вывести максимальное количество подряд идущих 1.*/
+
 public class task2 {
-    public static void main (String[] args) {
-        System.out.println("привет мир");
-       }
+    public static void main(String[] args) {
+        int arr[] = new int[] {1,1,0,1,0,1};
+        int temp = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1 ){
+               temp ++;
+    }
+        else {
+            if (temp > max) {
+               max = temp;
+    
+              }
+                   temp = 0;
     }
     
-    
-    
-}
+    }
+            if (temp > max) {
+                    max = temp;
+    }
+                System.out.println(max);
+    }
+    }
